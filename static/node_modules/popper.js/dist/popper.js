fc12f815e86940e5a704a3035d1d3d396a9ddf0a
Chuancong Gao <chuanconggao@users.noreply.github.com>
2017-11-11 15:49:49 -0800

    Update Yarn

--- static/node_modules/popper.js/dist/popper.js
+++ static/node_modules/popper.js/dist/popper.js
@@ -1,6 +1,6 @@
 /**!
  * @fileOverview Kickass library to create and place poppers near their reference elements.
- * @version 1.12.6
+ * @version 1.12.7
  * @license
  * Copyright (c) 2016 Federico Zivolo and contributors
  *
@@ -516,7 +516,7 @@ function getBoundaries(popper, reference, padding, boundariesElement) {
     // Handle other cases based on DOM element used as boundaries
     let boundariesNode;
     if (boundariesElement === 'scrollParent') {
-      boundariesNode = getScrollParent(getParentNode(popper));
+      boundariesNode = getScrollParent(getParentNode(reference));
       if (boundariesNode.nodeName === 'BODY') {
         boundariesNode = popper.ownerDocument.documentElement;
       }
